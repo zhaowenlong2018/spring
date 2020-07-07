49b214561b25d5137277703a11128d3e2f5fce70
zwl <735614289@qq.com>
2020-07-07 17:03:28 +0800

    Initialize Spring Project

---
+++ spring/springlifecycle/src/main/java/cn/zwl/spring/springlifecycle/SpringLifeCycleApplication.java
@@ -0,0 +1,13 @@
+package cn.zwl.spring.springlifecycle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLifeCycleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringLifeCycleApplication.class, args);
+    }
+
+}
---
+++ spring/springlifecycle/src/test/java/cn/zwl/spring/springlifecycle/SpringlifecycleApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.zwl.spring.springlifecycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringlifecycleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
